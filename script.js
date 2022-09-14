fb34abefe79aee347777c0e23a637b6766f3cbf3
CharlieH94 <94218945+CharlieH94@users.noreply.github.com>
2022-09-14 10:36:13 +0100

    index generator added

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+// Message components
 const jokeOptions = {
     responses: ["Who's there?", "Come in.", "Hello?", "Go Away."],
     whoIsIt: ['Mario', 'Tarzan', 'Professor X', 'Cortana'],
     descriptor: ['the Plumber', 'of the Jungle', 'the Mutant', 'UNSC Artificial Intelligence']
 }
 
+// Generate random number for index selection
+const randomIndexGenerator = (num) => Math.floor(Math.random() * num);
