6173baa56ac3729f21bd4ea5b830f2d08d3bc2ae
CharlieH94 <94218945+CharlieH94@users.noreply.github.com>
2022-09-14 10:29:27 +0100

    Message component arrays stored inside an object

---
+++ script.js
@@ -0,0 +1,6 @@
+const jokeOptions = {
+    responses: ["Who's there?", "Come in.", "Hello?", "Go Away."],
+    whoIsIt: ['Mario', 'Tarzan', 'Professor X', 'Cortana'],
+    descriptor: ['the Plumber', 'of the Jungle', 'the Mutant', 'UNSC Artificial Intelligence']
+}
+
