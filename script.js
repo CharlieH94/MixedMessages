96ca689b702520bbdbbbc6f19b6484cb275e5be0
CharlieH94 <94218945+CharlieH94@users.noreply.github.com>
2022-09-14 14:02:45 +0100

    message array, and final function created

--- script.js
+++ script.js
@@ -8,22 +8,34 @@ const jokeOptions = {
 // Generate random number for index selection
 const randomIndexGenerator = (num) => Math.floor(Math.random() * num);
 
-// Generate the random message components
+// Message array to use in function
+let jokeArray = [];
 
+// Generate the random message components
 for (let prop in jokeOptions) {
     let indexSelected = randomIndexGenerator(jokeOptions[prop].length);
     switch (prop) {
         case 'responses':
-            console.log(jokeOptions[prop][indexSelected]);
+            jokeArray.push(jokeOptions[prop][indexSelected]);
             break;
         case 'whoIsIt':
-            console.log(jokeOptions[prop][indexSelected]);
+            jokeArray.push(jokeOptions[prop][indexSelected]);
             break;
         case 'descriptor':
-            console.log(jokeOptions[prop][indexSelected]);
+            jokeArray.push(jokeOptions[prop][indexSelected]);
             break;
         default:
             break;
     }
 }
 
+// Putting the program together
+function rubbishJokeGenerator() {
+    console.log('Knock knock.');
+    console.log(`${jokeArray[0]}`);
+    console.log(`It's me, ${jokeArray[1]}!`);
+    console.log(`${jokeArray[1]} who?`);
+    console.log(`${jokeArray[1]} ${jokeArray[2]}.`);
+}
+
+rubbishJokeGenerator();
